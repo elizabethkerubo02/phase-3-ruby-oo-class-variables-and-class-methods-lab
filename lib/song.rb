a39bb739fab26d33b9ef3540e56e39433c9bc9b2
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-05-30 15:09:52 +0300

    completed

--- lib/song.rb
+++ lib/song.rb
@@ -35,23 +35,5 @@ class Song
 
 end
 
-# Creating instances of the Song class
-song1 = Song.new("Song 1", "Artist 1", "Genre 1")
-song2 = Song.new("Song 2", "Artist 2", "Genre 2")
-song3 = Song.new("Song 3", "Artist 1", "Genre 1")
 
-# Using the class methods to get information
-puts "Total number of songs: #{Song.count}"
-
-
-puts "Unique artists: #{Song.artists}"
-
-
-puts "Unique genres: #{Song.genres}"
-
-
-puts "Genre count: #{Song.genre_count}"
-
-
-puts "Artist count: #{Song.artist_count}"
 
